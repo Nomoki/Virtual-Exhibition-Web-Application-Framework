73d96353e48e99936c64a3b6b614477899978bd0
Nomoki <sroravitoak@hotmail.com>
2021-08-23 23:08:02 +0700

    camera control added
    add orcit control
    add resize window
    add cone

--- script.js
+++ script.js
@@ -1,28 +1,108 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.131.3';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.131.3/examples/jsm/controls/OrbitControls.js';
+
       
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let camera, controls, scene, renderer;
 
-			const renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+init();
+//render(); // remove when using next line for animation loop (requestAnimationFrame)
+animate();
 
-			const geometry = new THREE.BoxGeometry(1, 2, 1);
-			const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
-			const cube = new THREE.Mesh( geometry, material );
+function init() {
 
-            const size = 10;                                                 //grid size
-            const divisions = 15;
-            const gridHelper = new THREE.GridHelper( size, divisions );    //grid
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xcccccc );
 
-			scene.add( cube );
-            scene.add( gridHelper );
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-			camera.position.z = 10;
-            camera.position.y = 3;
-            
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 5, 10, 0 );
 
-    
-            renderer.render( scene, camera );
+    // controls
+
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.listenToKeyEvents( window ); // optional
+
+    //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.05;
+
+    controls.screenSpacePanning = false;
+
+    controls.minDistance = 5;
+    controls.maxDistance = 50;
+
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // lights
+
+  /*   const dirLight1 = new THREE.DirectionalLight( 0xffffff );
+    dirLight1.position.set( 1, 1, 1 );
+    scene.add( dirLight1 );
+
+    const dirLight2 = new THREE.DirectionalLight( 0x002288 );
+    dirLight2.position.set( - 1, - 1, - 1 );
+    scene.add( dirLight2 );
+
+    const ambientLight = new THREE.AmbientLight( 0x222222 );
+    scene.add( ambientLight ); */
+
+    //grid
+    const size = 10;
+    const divisions = 10;
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    scene.add( gridHelper );
+
+    //add cube
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xFF0000} );
+    const cube = new THREE.Mesh( geometry, material );
+    cube.position.y = 0.5;
+    scene.add( cube );
+
+    //add cone
+    const geometry2 = new THREE.ConeGeometry( 2, 3, 4 );
+    const material2 = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+    const cone = new THREE.Mesh( geometry2, material2 );
+    cone.position.y = 1.5;
+    cone.position.x = 3;
+    scene.add(cone);
+
+
+
+
+    // auto resize update
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+
+    render();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
 
 	
