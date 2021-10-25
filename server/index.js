c0534f0c54e6b59543a7f7f79bf293268a70c6d9
Puthinart-S <boatkakkakmak@gmail.com>
2021-10-26 02:44:04 +0700

    mern_side
    Auth with CRUD

---
+++ server/index.js
@@ -0,0 +1,25 @@
+
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import compression from 'compression';
+import postRoutes from './routes/posts.js';
+import userRouter from './routes/users.js';
+
+
+const app = express();
+
+app.use(compression());
+app.use(express.json({ limit: '1000mb', extended: true }))
+app.use(express.urlencoded({ limit: '1000mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+const CONNECTION_URL = 'mongodb+srv://Puthinart:Boat280520@vewafdb.s4toy.mongodb.net/vewafdb';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
