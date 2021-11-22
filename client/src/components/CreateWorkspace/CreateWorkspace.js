525f6717ecc13e8c48e2f03465a9c4040ad6b74f
Nomoki <sroravitoak@hotmail.com>
2021-11-23 01:47:10 +0700

    edit home
    change home to clear
    Add createworkspace

--- client/src/components/CreateWorkspace/CreateWorkspace.js
+++ client/src/components/CreateWorkspace/CreateWorkspace.js
@@ -1,10 +1,34 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
 
 const CreateWorkspace = () => {
+    const [currentId, setCurrentId] = useState(0);
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(getPosts());
+    }, [currentId, dispatch]);
+
     return (
         <Fragment>
-            
-        </Fragment>
+        <Grow in>
+            <Container>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    </Fragment>
     )
 }
 
